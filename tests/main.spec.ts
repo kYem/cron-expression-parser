aedc1a9d8d44ac9fe0ccad54d91c7c1a76803278
Kestutis Kasiulynas <k.kasiulynas@gmail.com>
2021-09-26 19:00:20 +0100

    Rename main to index.ts and export parser functions

--- tests/main.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import { main } from '../src/main';
-
-describe('main', function () {
-  it('should run', function () {
-    const result = main();
-    expect(result).to.be.ok
-  });
-});
